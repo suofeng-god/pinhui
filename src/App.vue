fe27d1738c5ce9dfb56247ec12d78cd6af614f51
suo <1002843368@qq.com>
2022-09-04 19:03:09 +0800

    home初步完成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div id="app">项目开始</div>
+  <div>
+    <Header></Header>
+    <!-- 路由组件出口 -->
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default {
   name: "App",
+  components: {
+    Header,
+    Footer,
+  },
 };
 </script>
 
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+// 配置路由
+import Vue from "vue";
+import VueRouter from "vue-router";
+// 需要使用插件
+Vue.use(VueRouter);
+// 配置路由
+
+// 引入路由组件
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import Search from "@/pages/Search";
+
+export default new VueRouter({
+  // 配置路由
+  routes: [
+    {
+      path: "/home",
+      component: Home,
+    },
+    {
+      path: "/login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      component: Register,
+    },
+    {
+      path: "/search",
+      component: Search,
+    },
+    // 重定向,在项目跑起来的时候,访问 / ,立马重定向到首页
+    {
+      path: "*",
+      redirect: "/home",
+    },
+  ],
+});
